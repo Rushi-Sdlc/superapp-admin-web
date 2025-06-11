57bdebe436cf11045c0188e61ebaadcd0d383e3d
Rushi-Sdlc <rushikeshvasande@sdlccorp.com>
2025-06-11 12:55:09 +0530

    setup

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,19 @@
-// src/services/api.ts
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com/' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://jsonplaceholder.typicode.com/',
+  }),
   endpoints: (builder) => ({
-    getPosts: builder.query<any[], void>({
+    getPosts: builder.query<Post[], void>({
       query: () => 'posts',
     }),
   }),
